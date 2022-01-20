15076884181d198c3a7ca6cb7b1ee54c10b2fdb0
miloszl13 <milosmihajlovic11111@gmail.com>
2022-01-20 11:39:17 +0100

    Add project files.

---
+++ XOX/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace XOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.txtPlayerOntTheMove = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(13, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 93);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(126, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 93);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(240, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 93);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightBlue;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(12, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 93);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightBlue;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(126, 138);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 92);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightBlue;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(240, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 92);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightBlue;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(13, 237);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 93);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LightBlue;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(127, 238);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 92);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.LightBlue;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.Location = new System.Drawing.Point(240, 237);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(107, 92);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.buttons_click);
+            // 
+            // txtPlayerOntTheMove
+            // 
+            this.txtPlayerOntTheMove.Location = new System.Drawing.Point(12, 12);
+            this.txtPlayerOntTheMove.Name = "txtPlayerOntTheMove";
+            this.txtPlayerOntTheMove.Size = new System.Drawing.Size(335, 20);
+            this.txtPlayerOntTheMove.TabIndex = 9;
+            this.txtPlayerOntTheMove.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(359, 344);
+            this.Controls.Add(this.txtPlayerOntTheMove);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "XOX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox txtPlayerOntTheMove;
+    }
+}
+
---
+++ XOX/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOX
+{
+    public partial class Form1 : Form
+    {
+        //variable that stores the sign of player that is on the move
+        private string playeronthemove = "X";
+        //variable that shows if someone won the game
+        private bool someoneWon = false;
+        public Form1()
+        {
+            InitializeComponent();
+            //Text shown in text box on the begining
+            txtPlayerOntTheMove.Text= "PLAYER X IS PLAYING NOW ";
+        }
+
+        //event after click on any button
+        private void buttons_click(object sender,EventArgs e)
+        {
+            var button = (Button)sender;
+            //if player x is on move and this button text is clear
+            if (playeronthemove == "X" && button.Text == "" && someoneWon==false)
+            {
+                //button text will be "X"
+                button.Text = "X";
+                //if any of this options is true it is the end of the game and app will show us message box
+                if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X" ||
+                   button2.Text == "X" && button5.Text == "X" && button8.Text == "X" ||
+                   button3.Text == "X" && button6.Text == "X" && button9.Text == "X" ||
+                   button1.Text == "X" && button2.Text == "X" && button3.Text == "X" ||
+                   button4.Text == "X" && button5.Text == "X" && button6.Text == "X" ||
+                   button7.Text == "X" && button8.Text == "X" && button9.Text == "X" ||
+                   button1.Text == "X" && button5.Text == "X" && button9.Text == "X" ||
+                   button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+                {
+                    someoneWon = true;
+                    //in message box we have options to click yes or no
+                    DialogResult dialogResult = MessageBox.Show("PLAYER X WON!!! \n Do you want to play again??", "END OF THE GAME", MessageBoxButtons.YesNo);
+                    //if we click on yes,text of all buttons will be empty string and player on the move will be player X
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        button1.Text = "";
+                        button2.Text = "";
+                        button3.Text = "";
+                        button4.Text = "";
+                        button5.Text = "";
+                        button6.Text = "";
+                        button7.Text = "";
+                        button8.Text = "";
+                        button9.Text = "";
+                        playeronthemove = "X";
+                    }
+                    //if we click on no the application will close
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        System.Diagnostics.Process.GetCurrentProcess().Kill();
+                    }
+                }
+                else if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != ""
+                && button7.Text != "" && button8.Text != "" && button9.Text != "" && someoneWon == false)
+                {
+                    //in message box we have options to click yes or no
+                    DialogResult dialogResult = MessageBox.Show("ITS A DRAW!!! \n Do you want to play again??", "END OF THE GAME", MessageBoxButtons.YesNo);
+                    //if we click on yes,text of all buttons will be empty string and player on the move will be player X
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        button1.Text = "";
+                        button2.Text = "";
+                        button3.Text = "";
+                        button4.Text = "";
+                        button5.Text = "";
+                        button6.Text = "";
+                        button7.Text = "";
+                        button8.Text = "";
+                        button9.Text = "";
+                        playeronthemove = "X";
+                    }
+                    //if we click on no the application will close
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        System.Diagnostics.Process.GetCurrentProcess().Kill();
+                    }
+                }
+                //if it is not the end of the game ,player Y is on the move
+                else
+                {
+                    playeronthemove = "Y";
+                    txtPlayerOntTheMove.Text = "PLAYER Y IS PLAYING NOW ";
+                }
+            }
+            //If player Y is on move and this button text is clear
+            else if (playeronthemove == "Y" && button.Text == "" && someoneWon==false)
+            {
+                //button text will be "Y"
+
+                button.Text = "Y";
+                //if any of this options is true it is the end of the game and app will show us message box
+                if (button1.Text == "Y" && button4.Text == "Y" && button7.Text == "Y" ||
+                    button2.Text == "Y" && button5.Text == "Y" && button8.Text == "Y" ||
+                    button3.Text == "Y" && button6.Text == "Y" && button9.Text == "Y" ||
+                    button1.Text == "Y" && button2.Text == "Y" && button3.Text == "Y" ||
+                    button4.Text == "Y" && button5.Text == "Y" && button6.Text == "Y" ||
+                    button7.Text == "Y" && button8.Text == "Y" && button9.Text == "Y" ||
+                    button1.Text == "Y" && button5.Text == "Y" && button9.Text == "Y" ||
+                    button3.Text == "Y" && button5.Text == "Y" && button7.Text == "Y")
+                {
+                    someoneWon = true;
+                    //in message box we have options to click yes or no
+                    DialogResult dialogResult = MessageBox.Show("PLAYER Y WON!!! \n Do you want to play again??", "END OF THE GAME", MessageBoxButtons.YesNo);
+                    //if we click on yes,text of all buttons will be empty string and player on the move will be player X
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        button1.Text = "";
+                        button2.Text = "";
+                        button3.Text = "";
+                        button4.Text = "";
+                        button5.Text = "";
+                        button6.Text = "";
+                        button7.Text = "";
+                        button8.Text = "";
+                        button9.Text = "";
+                        playeronthemove = "X";
+                        someoneWon = false;
+                    }
+                    //if we click on no the application will close
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        System.Diagnostics.Process.GetCurrentProcess().Kill();
+                    }
+                }
+                else if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != ""
+                && button7.Text != "" && button8.Text != "" && button9.Text != "" && someoneWon == false)
+                {
+                    //in message box we have options to click yes or no
+                    DialogResult dialogResult = MessageBox.Show("ITS A DRAW!!! \n Do you want to play again??", "END OF THE GAME", MessageBoxButtons.YesNo);
+                    //if we click on yes,text of all buttons will be empty string and player on the move will be player X
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        button1.Text = "";
+                        button2.Text = "";
+                        button3.Text = "";
+                        button4.Text = "";
+                        button5.Text = "";
+                        button6.Text = "";
+                        button7.Text = "";
+                        button8.Text = "";
+                        button9.Text = "";
+                        playeronthemove = "X";
+                        someoneWon = false;
+                    }
+                    //if we click on no the application will close
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        System.Diagnostics.Process.GetCurrentProcess().Kill();
+                    }
+                }
+                else
+                {
+                    playeronthemove = "X";
+                    txtPlayerOntTheMove.Text = "PLAYER X IS PLAYING NOW ";
+                }
+            }
+           
+            else
+            {
+                MessageBox.Show("Error");
+            }
+                
+            
+
+        }
+
+    }
+}
